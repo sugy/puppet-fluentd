e48e702ac84ba616782aabc3aa76e1171e0a5181
Nick Maludy <nmaludy@gmail.com>
2020-11-12 22:56:18 -0500

    Renamed provider types for package

--- lib/puppet/provider/package/tdagent.rb
+++ lib/puppet/provider/package/tdagent.rb
@@ -1,5 +1,24 @@
 Puppet::Type.type(:package).provide :tdagent, parent: :gem, source: :gem do
   has_feature :install_options, :versionable
-  confine false: (Puppet[:operatingsystem] == :windows)
-  commands gemcmd: '/opt/td-agent/usr/sbin/td-agent-gem'
+
+  # yes, i know this isn't how you're "supposed" to do it, but because
+  # we're overriding the "gem" package provider here, we need this to be
+  # one named provider for use in our package resources.
+  if Puppet::Util::Platform.windows?
+    if Puppet::FileSystem.exist?('C:/opt/td-agent/bin/fluent-gem.bat')
+      # v4 and newer
+      commands gemcmd: 'C:/opt/td-agent/bin/fluent-gem.bat'
+    else
+      # v3 and older
+      commands gemcmd: 'C:/opt/td-agent/embedded/bin/fluent-gem.bat'
+    end
+  else
+    if Puppet::FileSystem.exist?('/usr/sbin/td-agent-gem')
+      # v3, v4 and newer
+      commands gemcmd: '/usr/sbin/td-agent-gem'
+    else
+      # v0
+      commands gemcmd: '/opt/td-agent/usr/sbin/td-agent-gem'
+    end
+  end
 end
--- lib/puppet/provider/package/tdagent_v3_windows.rb
+++
@@ -1,5 +0,0 @@
-Puppet::Type.type(:package).provide :tdagent, parent: :gem, source: :gem do
-  has_feature :install_options, :versionable
-  confine operatingsystem: :windows
-  commands gemcmd: 'C:/opt/td-agent/embedded/bin/fluent-gem.bat'
-end
